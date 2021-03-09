f07b31493487f58eba44b230fe0710d0e9391145
Haddad <emha.haddad@gmail.com>
2021-03-10 01:50:50 +0700

    fix bug for deployment

--- base/settings.py
+++ base/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2uryr62_ntcbdgg(6wpbvffn)eu2o+m-+mso-&(7#k)u39)x6%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
